9cc140e5de10e6dc221ecd6ce22a1a7221088c7a
unknown <marcosviniciosampaioaraujo@gmail.com>
2024-05-21 13:39:59 -0300

    Put/Update

--- teste_tecnico.py
+++ teste_tecnico.py
@@ -8,12 +8,16 @@ conexao = mysql.connector.connect(
 )
 cursor = conexao.cursor()
 class Vendedor:
-    #POST
+    #POST/CREATE
     comando = f"INSERT INTO vendedores (nome, cpf, data_nascimento, email, estado) VALUES ('João', '11109890202', '2001/09/09', 'joao@gmail.com', 'SC')"
     cursor.execute(comando)
     conexao.commit()
-    #GET
+    #GET/READ
     comando = f'SELECT * FROM vendedores'
     cursor.execute(comando)
     resultado = cursor.fetchall()
     print(resultado)
+    #PUT/UPDATE
+    comando = f'UPDATE vendedores SET email = ("novoemail@gmail.com") WHERE nome = "João"'
+    cursor.execute(comando)
+    conexao.commit() # edita o banco de dados
