4f2b9b2bd9eb97ba747725903566f26895ce7d69
unknown <marcosviniciosampaioaraujo@gmail.com>
2024-05-22 14:02:29 -0300

    post update

--- CRUD.py
+++ CRUD.py
@@ -7,16 +7,32 @@ conexao = mysql.connector.connect(
     database='crud',
 )
 cursor = conexao.cursor()
+
 class Vendedor:
-    #POST/CREATE
-    comando = f"INSERT INTO vendedores (nome, cpf, data_nascimento, email, estado) VALUES ('João', '11109890202', '2001/09/09', 'joao@gmail.com', 'SC')"
-    cursor.execute(comando)
-    conexao.commit()
+    # POST/CREATE
+    def post(self, nome, cpf, data_nascimento, email, estado):
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+        self.email = email
+        self.estado = estado
+        comando = "INSERT INTO vendedores (nome, cpf, data_nascimento, email, estado) VALUES (%s, %s, %s, %s, %s)"
+        valores = (self.nome, self.cpf, self.data_nascimento, self.email, self.estado)
+        cursor.execute(comando, valores)
+        conexao.commit()
+
+
+vendedor = Vendedor()
+
+vendedor.post('marcos', '11110989997', '2001/09/09', 'marcos@gmail.com', 'SP')
+
+'''
     #GET/READ
     comando = f'SELECT * FROM vendedores'
     cursor.execute(comando)
     resultado = cursor.fetchall()
     print(resultado)
+
     #PUT/UPDATE
     comando = f'UPDATE vendedores SET email = ("novoemail@gmail.com") WHERE nome = "João"'
     cursor.execute(comando)
@@ -25,4 +41,4 @@ class Vendedor:
     comando = f'DELETE FROM vendedores WHERE nome = "João"'
     cursor.execute(comando)
     conexao.commit()
-
+'''
